96e2aa55e8add82ef8a5f2e4110f53e0b01b332a
ZanSara <sarazanzo94@gmail.com>
2015-03-31 20:21:26 +0800

    first models created

--- twdproject/rango/models.py
+++ twdproject/rango/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+# NOTE Django automatically creates the primary key
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    def __str__(self):
+	return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    viewed = models.IntegerField(default=0)
+    def __str__(self):
+	return self.title
+
--- twdproject/twdproject/urls.py
+++ twdproject/twdproject/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Matches a URL that begins with admin/, then
@@ -7,3 +8,17 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
 )
+
+# For any file requested with a URL starting with /media,
+# the request will be passed to the django.views.static view,
+# which handles the upload of the file.
+#
+# WARNING! To be used only during development. On deployement,
+# find a better solution!
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}),
+        )
