c3181c04fd3f5be31379b42556b7f4c022136be6
ZanSara <sarazanzo94@gmail.com>
2015-04-04 23:55:17 +0800

    database & admin page

--- twdproject/rango/admin.py
+++ twdproject/rango/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+# Note the typo within the admin interface
+# (categorys, not categories). This problem
+# can be fixed by adding a nested Meta class
+# into your model definitions with the
+# verbose_name_plural attribute. Check out
+# Django official documentation on models
+# for more information.
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
--- twdproject/rango/views.py
+++ twdproject/rango/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 def index(request):
-    template_dict={'boldmessage':'I am from the dictionary'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    template_dict={'categories':category_list}
     return render(request, 'rango/index.html', template_dict)
 
 def about(request):
