e86d067faef5a222b99e3e4b640d0c95979d9ada
ZanSara <sarazanzo94@gmail.com>
2015-04-06 14:54:43 +0800

    forms created and working

---
+++ twdproject/rango/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from rango.models import Page, Category
+
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=100, help_text='Type the category name')
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+    # Meta is an inline class that provides additional information on the form
+    # Here links the ModelForm and its Model
+
+    class Meta:
+	model = Category
+	fields = ('name',)
+
+
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=100, help_text='Type the page title')
+    url = forms.URLField(max_length=200, help_text='Paste here the page''s URL')
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        # If url is not empty and doesn't start with 'http://', adds 'http://'.
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+        return cleaned_data
+
+    class Meta:
+	model = Page
+	# We can either exclude the category field from the form,
+        exclude = ('category',)
+        # or specify the fields to include
+        #fields = ('title', 'url', 'views')
+
--- twdproject/rango/views.py
+++ twdproject/rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -27,4 +28,40 @@ def category(request, category_name_slug):
 	# The template will show a "No such Category": no need to set anything
 	pass
     return render(request, 'rango/category.html', template_context)
-	
+
+
+def add_category(request):
+    if request.method == 'POST':
+	form = CategoryForm(request.POST)
+	if form.is_valid():
+	    form.save(commit=True)
+	    # Call the index view to show the index page
+	    return index(request)
+	else:
+	    # Simply print the errors in the terminal
+	    print form.errors
+    else:
+	form = CategoryForm()
+    # Render the form page
+    return render(request, 'rango/add_category.html', {'form':form})
+
+def add_page(request, category_name_slug):
+    try:
+	sel_category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+	sel_category = None
+    if request.method == 'POST':
+	form = PageForm(request.POST)
+	if form.is_valid():
+	    if sel_category:
+		page = form.save(commit=False)
+		page.category = sel_category
+		page.views = 0
+		page.save()
+		return category(request, category_name_slug)
+	else:
+	    print form.errors
+    else:
+	form = PageForm()
+    template_context = {'form':form, 'category':sel_category}
+    return render(request, 'rango/add_page.html', template_context)
