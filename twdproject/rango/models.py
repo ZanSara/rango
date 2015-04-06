cfdf15d1e2fa6daa30cf797da1296b26a4b91400
ZanSara <sarazanzo94@gmail.com>
2015-04-06 14:00:17 +0800

    category page created

--- twdproject/rango/models.py
+++ twdproject/rango/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.template.defaultfilters import slugify
 
 # NOTE Django automatically creates the primary key
 
@@ -7,8 +7,15 @@ class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+    
+    def save(self, *args, **kwargs):
+	self.slug = slugify(self.name)
+	super(Category, self).save(*args, **kwargs)
+	
     def __str__(self):
 	return self.name
+
 
 class Page(models.Model):
     category = models.ForeignKey(Category)
--- twdproject/rango/views.py
+++ twdproject/rango/views.py
@@ -4,8 +4,8 @@ from rango.models import Category, Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
-    template_dict={'categories':category_list}
-    return render(request, 'rango/index.html', template_dict)
+    template_context={'categories':category_list}
+    return render(request, 'rango/index.html', template_context)
 
 def about(request):
     return HttpResponse("""What about Rango?
@@ -14,3 +14,17 @@ def about(request):
 			<br>
 			<a href='/rango'>Home</a>
 			""")
+
+def category(request, category_name_slug):
+    template_context={}
+    try:
+	sel_category = Category.objects.get(slug=category_name_slug)
+	pages = Page.objects.filter(category=sel_category)
+	template_context['category_name'] = sel_category.name
+	template_context['category'] = sel_category
+	template_context['pages'] = pages
+    except Category.DoesNotExist:
+	# The template will show a "No such Category": no need to set anything
+	pass
+    return render(request, 'rango/category.html', template_context)
+	
